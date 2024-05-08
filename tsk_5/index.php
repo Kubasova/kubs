4ec258f977d41fb3fc431926e148a7deead0b580
Galettos <144013403+Kubasova@users.noreply.github.com>
2024-05-08 19:00:10 +0300

    Update index.php

--- tsk_5/index.php
+++ tsk_5/index.php
@@ -98,7 +98,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $userLogin = $_SESSION['login'];
 
   $user = 'u67329';
-  $pass = '7646979';
+  $pass = '6746979';
   $db = new PDO('mysql:host=localhost;dbname=u67329', $user, $pass,
   [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
