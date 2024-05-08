0d07372ffd57702e25134b1286cd831d361b0186
Galettos <144013403+Kubasova@users.noreply.github.com>
2024-05-08 19:11:23 +0300

    Update admin.php

--- tsk_6/admin.php
+++ tsk_6/admin.php
@@ -9,7 +9,7 @@
 // Подробнее см. стр. 26 и 99 в учебном пособии Веб-программирование и веб-сервисы.
 
 $user = 'u67329';
-$pass = '7646979';
+$pass = '6746979';
 $db = new PDO('mysql:host=localhost;dbname=u67329', $user, $pass,
   [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
