f067e0727eb15a6a8c967d0fc79764d5d212a65b
Galettos <144013403+Kubasova@users.noreply.github.com>
2024-06-04 23:16:49 +0300

    Update editOutputLog.php

--- practic/site/editOutputLog.php
+++ practic/site/editOutputLog.php
@@ -9,40 +9,40 @@
 <body>
     <div class="container">
         <div class="block">
-            <h2>Добавить средство</h2>
+            <h2>Изменить смену</h2>
             <form action="" method="POST">
-                <fieldset>
-                <legend>Выберите артиста:</legend>
-                <?php foreach ($artists as $artist) :?>
-                    <div>
-                        <label for="<?php echo $artist["first_name"];?>">
-                        <input type="radio" name="artist_id" value="<?php echo $artist["artist_id"];?>" />
-                        <?php echo $artist["first_name"];?>
-                        </label>
-                    </div>
-                    <?php endforeach;?>
-                </fieldset>
-                <fieldset>
-                    <legend>Выберите шоу:</legend>
-                    <?php foreach ($areas as $area) :?>
-                        <div>
-                            <label for="<?php echo $area["name"];?>">
-                            <input type="radio" name="venue_id" value="<?php echo $area["venue_id"];?>" />
-                            <?php echo $area["name"];?>
-                            </label>
-                        </div>
-                    <?php endforeach;?>
-                </fieldset>
                 <div class="form-group">
                     <label for="date_beg">Начиная с:</label>
-                    <input type="date" id="date_beg" name="date_beg">
+                    <input type="date" id="date_beg"  value="<?php echo $currentLog['date_beg']; ?>"name="date_beg">
                 </div>
                 <div class="form-group">
                     <label for="date_end">До:</label>
-                    <input type="date" id="date_end" name="date_end">
+                    <input type="date" id="date_end"  value="<?php echo $currentLog['date_end']; ?>"name="date_end">
                 </div>
-                <input type="text" id="perf_id" value="<?php echo $currentLog["perf_id"];?>" name="perf_id" hidden>
-                <input type="submit" value="Редактировать журнал" name="UpdateOutputlogs">
+                <fieldset>
+            <legend>Выберите артиста:</legend>
+            <?php foreach ($artists as $artist) :?>
+            <div>
+                <label for="<?php echo $artist["first_name"];?>">
+                <input type="radio" name="artist_id" value="<?php echo $artist["artist_id"];?>" />
+                <?php echo $artist["first_name"];?>
+                </label>
+            </div>
+            <?php endforeach;?>
+            </fieldset>
+            <fieldset>
+            <legend>Выберите Шоу:</legend>
+            <?php foreach ($areas as $area) :?>
+            <div>
+                <label for="<?php echo $area["name"];?>">
+                <input type="radio" name="venue_id" value="<?php echo $area["venue_id"];?>" />
+                <?php echo $area["name"];?>
+                </label>
+            </div>
+            <?php endforeach;?>
+            </fieldset>
+            <input type="text" id="perf_id" value="<?php echo $currentLog["perf_id"];?>" name="perf_id" hidden>
+                <input type="submit" value="Применить" name="UpdateOutputlogs">
             </form>
         </div>
     </div>
